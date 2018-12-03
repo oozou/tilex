2a5795938b920f63c3106e613fbbd4a56c5e909d
Alireza Bashiri <azbshiri@gmail.com>
2018-12-03 17:08:15 +0700

    Remove all channels before seeding the database
    What?
    This prevents from duplicate errors during setup which causes a infinite
    loop in pm2.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,8 +1,10 @@
 alias Tilex.{Repo, Channel}
 
+Repo.delete_all(Channel)
+
 channels = ["ruby", "react", "javascript", "testing", "phoenix", "elixir", "vim", "erlang", "nodejs"]
 
 channels
 |> Enum.each(fn channel ->
-  Repo.insert(%Channel{name: channel, twitter_hashtag: channel})
+  Repo.insert!(%Channel{name: channel, twitter_hashtag: channel})
 end)
