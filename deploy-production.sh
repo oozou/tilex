8e6f7b2a91cc083200f162e2109e2e716a5a8653
consti <consti@consti.de>
2018-12-04 15:53:04 +0700

    Auto backup DB

--- deploy-production.sh
+++ deploy-production.sh
@@ -2,8 +2,10 @@
 
 ssh slack_bot@til.oozou.com << EOF
   cd /var/www/til.oozou.com/
-  echo "updating repository ✓" && \
+  echo "backing up DB ✅" && \
+  pg_dump -d til -f db/backups/til-production-$(date +"%Y-%m-%dT%H:%M:%S").sql
+  echo "updating repository ✅" && \
   GIT_SSH_COMMAND='ssh -i /home/slack_bot/.ssh/til' git fetch --all > /dev/null 2>&1 && \
   GIT_SSH_COMMAND='ssh -i /home/slack_bot/.ssh/til' git reset --hard origin/master > /dev/null 2>&1
-  echo "restarting the server ✓" && pm2 restart til > /dev/null 2>&1
+  echo "restarting the server ✅" && pm2 restart til > /dev/null 2>&1
 EOF
