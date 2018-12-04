4f66c51ff4cabac83dc44f8a68e89d3b3306c5ad
consti <consti@consti.de>
2018-12-04 15:19:35 +0700

    use port 80 for external links, even though it is 443 (redirect handled by nginx)

--- config/prod.exs
+++ config/prod.exs
@@ -8,7 +8,7 @@ use Mix.Config
 # with brunch.io to recompile .js and .css sources.
 config :tilex, TilexWeb.Endpoint,
   http: [port: {:system, "PORT"}, compress: true],
-  url: [host: System.get_env("HOST"), port: 443],
+  url: [host: System.get_env("HOST"), port: 80],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
